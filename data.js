05e1aa33d5704bb5e27020e1882b2e94088e99f3
zohebvirani <46392532+zohebvirani@users.noreply.github.com>
2019-01-31 11:38:49 -0800

    Update data.js
    Added Shows through the end of Q1 on 1/31/2019

--- data.js
+++ data.js
@@ -7,16 +7,76 @@ const data = {
     //   location: 'StageWorks'
     // },
     {
-      title: 'Driftstone Improv Show',
-      date: '1/16/2019',
-      time: '21:00', // military time
-      location: 'StageWerx'
+      title: 'Zo @ Bar Fluxus',
+      date: '2/2/2019',
+      time: '19:00', // military time
+      location: 'Bar Fluxus'
     },
     {
-      title: 'Some New Show',
-      date: '2/16/2019',
-      time: '22:00', // military time
-      location: 'Mars'
+      title: 'Drifstone @ Harold Night',
+      date: '2/6/2019',
+      time: '21:00', // military time
+      location: 'Stage Werx Theatre'
+    },
+    {
+      title: 'Zipper Dipper in Divination Intervention - Improv ',
+      date: '2/6/2019',
+      time: '19:30', // military time
+      location: 'ETC South'
+    },    
+    {
+      title: 'Zo @ The Set Up ',
+      date: '2/6/2019',
+      time: '19:30', // military time
+      location: 'ETC South'
+    },
+    {
+      title: 'Drifstone @ Harold Night',
+      date: '2/20/2019',
+      time: '21:00', // military time
+      location: 'Stage Werx Theatre'
+    },
+    {
+      title: 'Zipper Dipper in Divination Intervention - Improv ',
+      date: '2/22/2019',
+      time: '19:30', // military time
+      location: 'ETC South'
+    },
+    {
+      title: 'Drifstone @ Harold Night',
+      date: '2/27/2019',
+      time: '21:00', // military time
+      location: 'Stage Werx Theatre'
+    },
+    {
+      title: 'Zipper Dipper in Divination Intervention - Improv ',
+      date: '3/8/2019',
+      time: '19:30', // military time
+      location: 'ETC South'
+    },
+    {
+      title: 'Drifstone @ Harold Night',
+      date: '3/13/2019',
+      time: '21:00', // military time
+      location: 'Stage Werx Theatre'
+    },
+    {
+      title: 'Drifstone @ Harold Night',
+      date: '3/20/2019',
+      time: '21:00', // military time
+      location: 'Stage Werx Theatre'
+    },
+    {
+      title: 'Zipper Dipper in Divination Intervention - Improv ',
+      date: '3/22/2019',
+      time: '19:30', // military time
+      location: 'ETC South'
+    },
+    {
+      title: 'Zo @ Rooster T Feathers Competition ',
+      date: '3/27/2019',
+      time: '20:00', // military time
+      location: 'Rooster T Feathers'
     },
     
   ]
